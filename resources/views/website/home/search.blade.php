9981acb854366344fa3d446f3c621b510b300fb4
haidi <haidinurhadinata@gmail.com>
2019-10-16 20:38:05 +0800

    #37 kasih custom css pada form search

--- resources/views/website/home/search.blade.php
+++ resources/views/website/home/search.blade.php
@@ -6,6 +6,10 @@
         {{-- <div class="center-search"> --}}
             <h2>Welcome To Kalimantan Timur</h2>
             <h4>We offer a variety of services and options</h4>
+            <form role="search" class="search-form">
+                <input type="text" id="search" name="search" placeholder="what are you looking for?">
+                <button type="submit" id="search-submit"><i class="fa fa-search"></i></button>
+            </form>
 
         {{-- </div> --}}
 
