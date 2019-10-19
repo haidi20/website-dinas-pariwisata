fa5e3980b2481da7981a1f8064298c3b25378061
haidi <haidinurhadinata@gmail.com>
2019-10-19 11:23:28 +0800

    #37 update source code

--- public/0.js
+++ public/0.js
@@ -1,1 +1,25 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{10:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return u}));var r=e(0),c=e.n(r);function u(n){return c.a.createElement("img",{src:n.src,alt:""})}}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./resources/js/components/atoms/Image.js":
+/*!************************************************!*\
+  !*** ./resources/js/components/atoms/Image.js ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Image; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+function Image(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: props.src,
+    alt: ""
+  });
+}
+
+/***/ })
+
+}]);
