e7eb3c964e33ca91d4ac0e9a33fd3e2c28b1c7c8
egin10 <egin066@gmail.com>
2019-10-19 09:50:40 +0700

    add layout sitemanager in react

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 //sitemanager
+import AppSiteManager from './pages/SiteManager';
 
+//app-sitemanager
+if (document.getElementById('app-sitemanager')) {
+    ReactDOM.render(<AppSiteManager />, document.getElementById('app-sitemanager'));
+}
 
 //website
 import Home from './pages/Website/Home';
