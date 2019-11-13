7200992366584d5723eea443116e225e2e1a3a30
haidi <haidinurhadinata@gmail.com>
2019-11-13 08:31:04 +0800

    menambahkan link share medsos di detail post

--- resources/views/website/post/detail.blade.php
+++ resources/views/website/post/detail.blade.php
@@ -80,7 +80,13 @@
                             <div class="share-post-box">
                                 <ul class="share-box">
                                     @foreach ($shares as $index => $item)
-                                        <li><a class="{{$item->name == 'google-plus' ? 'google' : $item->name}}" href="#"><i class="fa fa-{{$item->name}}" style="position:center"></i></a></li>
+                                        <li>
+                                            <a 
+                                                class="{{$item->name}}" 
+                                                href="{{$item->setShareMedsos(Request::url())}}"
+                                                target="_blank"
+                                            ><i class="fa fa-{{$item->name}}" style="position:center"></i>
+                                        </a></li>
                                     @endforeach
                                 </ul>
                             </div>
