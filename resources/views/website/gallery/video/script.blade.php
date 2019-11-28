86418a87dd6df2e31e0b87a535df8f9353cde64a
haidi <haidinurhadinata@gmail.com>
2019-11-28 20:27:42 +0800

    sudah bisa insert dan tampilkan datanya

--- resources/views/website/gallery/video/script.blade.php
+++ resources/views/website/gallery/video/script.blade.php
@@ -48,6 +48,7 @@
             // Handle the results here (response.result has the parsed body).
             console.log("Response", response);
             var author = response.result.snippet.topLevelComment.snippet
+            var comments = '';
 
             var myUserEntity = {};
             myUserEntity.name   = author.authorDisplayName
@@ -58,6 +59,12 @@
             sessionStorage.setItem('myUserEntity', JSON.stringify(myUserEntity));
             // console.log('session = '+sessionStorage.getItem('myUserEntity'))
             // console.log('data = '+myUserEntity.name)
+            $('.comment').val('');
+            $('.send-comment').addClass('disabled btn-info');
+
+            comments = insert_comment(response.result, comments);
+            console.log(comments)
+            $('#list-comments').prepend(comments);
         },
         function(err) { console.error("Execute error", err); });
     }
@@ -109,8 +116,63 @@
 
         // console.log('session = '+sessionStorage.getItem('myUserEntity'))
 
-        // $('#comment-form').show();
+        show_comments();
     });
+
+    function show_comments()
+    {
+        let comments = '';
+
+        url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={{explode("v=",$video->link)[1]}}&key=AIzaSyDHy18clWdaVSoLKhwxAI24XSotdc-Ucqo';
+        $.ajax({
+            url: url,
+            type: 'get',
+            success:function(response){
+                response.items.map(comment => {
+                    // console.log(comment.snippet);
+
+                    comments = insert_comment(comment, comments);
+                });
+
+                $('#list-comments').append(comments);
+                $('#comment_thread_id').val(response.items[0].id);
+            },
+            error: function(xhr, ajaxOptions, thrownError){
+                console.log(xhr, ajaxOptions, thrownError)
+            }
+        });
+    }
+
+    function insert_comment(comment, comments)
+    {
+        const bulan = [ 'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agust', 'Sept', 'Okt', 'Nov', 'Des' ];
+
+        var date = new Date(comment.snippet.topLevelComment.snippet.updatedAt).getDate();
+        var month = bulan[new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMonth()];
+        var year = (new Date(comment.snippet.topLevelComment.snippet.updatedAt).getYear() < 1000) ? new Date(comment.snippet.topLevelComment.snippet.updatedAt).getYear() + 1900 : new Date(comment.snippet.topLevelComment.snippet.updatedAt).getYear();
+        var hours = (new Date(comment.snippet.topLevelComment.snippet.updatedAt).getHours() > 10) ? new Date(comment.snippet.topLevelComment.snippet.updatedAt).getHours() : '0' + new Date(comment.snippet.topLevelComment.snippet.updatedAt).getHours();
+        var minutes = (new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMinutes() > 10) ? new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMinutes() : '0' + new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMinutes();
+        var time = hours+':'+minutes;
+        var date_time = date +' '+month+' '+year+' '+'  '+time;
+
+        comments = comments +'<li class="place-comment">';
+            comments = comments+'<div id="avatar_id">'; 
+                comments = comments+'<img class="image-comment" src="'+comment.snippet.topLevelComment.snippet.authorProfileImageUrl+'" alt="user_image">';
+            comments = comments+'</div>';
+            comments = comments+'<div class="post-content">';
+                comments = comments+'<div class="name">';
+                    comments = comments+'<h2>'+comment.snippet.topLevelComment.snippet.authorDisplayName+'</h2> <p>'+date_time+'</p>';
+                comments = comments+'</div>';
+                comments = comments+'<div class="comment">';
+                    comments = comments+'<p>';
+                        comments = comments+comment.snippet.topLevelComment.snippet.textOriginal;
+                    comments = comments+'</p>';
+                comments = comments+'</div>';
+            comments = comments+'</div>';
+        comments = comments+'</li>';
+
+        return comments;
+    }
 
     // function onSignIn(googleUser) {
     //     var profile = googleUser.getBasicProfile();
@@ -195,59 +257,6 @@
     //     },function(err) { console.error("Error loading GAPI client for API", err); });
     // }
 
-    // function show_comments()
-    // {
-    //     let comments = '';
-
-    //     url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={{explode("v=",$video->link)[1]}}&key=AIzaSyDHy18clWdaVSoLKhwxAI24XSotdc-Ucqo';
-    //     $.ajax({
-    //         url: url,
-    //         type: 'get',
-    //         success:function(response){
-    //             response.items.map(comment => {
-    //                 // console.log(comment.snippet);
-
-    //                 comments = insert_comment(comment, comments);
-    //             });
-
-    //             $('#list-comments').append(comments);
-    //             $('#comment_thread_id').val(response.items[0].id);
-    //         },
-    //         error: function(xhr, ajaxOptions, thrownError){
-    //             console.log(xhr, ajaxOptions, thrownError)
-    //         }
-    //     });
-    // }
-
-    // function insert_comment(comment, comments)
-    // {
-    //     const bulan = [ 'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agust', 'Sept', 'Okt', 'Nov', 'Des' ];
-
-    //     var date = new Date(comment.snippet.topLevelComment.snippet.updatedAt).getDate();
-    //     var month = bulan[new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMonth()];
-    //     var year = (new Date(comment.snippet.topLevelComment.snippet.updatedAt).getYear() < 1000) ? new Date(comment.snippet.topLevelComment.snippet.updatedAt).getYear() + 1900 : new Date(comment.snippet.topLevelComment.snippet.updatedAt).getYear();
-    //     var hours = (new Date(comment.snippet.topLevelComment.snippet.updatedAt).getHours() > 10) ? new Date(comment.snippet.topLevelComment.snippet.updatedAt).getHours() : '0' + new Date(comment.snippet.topLevelComment.snippet.updatedAt).getHours();
-    //     var minutes = (new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMinutes() > 10) ? new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMinutes() : '0' + new Date(comment.snippet.topLevelComment.snippet.updatedAt).getMinutes();
-    //     var time = hours+':'+minutes;
-    //     var date_time = date +' '+month+' '+year+' '+'  '+time;
-
-    //     comments = comments +'<li class="place-comment">';
-    //         comments = comments+'<div id="avatar_id">'; 
-    //             comments = comments+'<img class="image-comment" src="'+comment.snippet.topLevelComment.snippet.authorProfileImageUrl+'" alt="user_image">';
-    //         comments = comments+'</div>';
-    //         comments = comments+'<div class="post-content">';
-    //             comments = comments+'<div class="name">';
-    //                 comments = comments+'<h2>'+comment.snippet.topLevelComment.snippet.authorDisplayName+'</h2> <p>'+date_time+'</p>';
-    //             comments = comments+'</div>';
-    //             comments = comments+'<div class="comment">';
-    //                 comments = comments+'<p>';
-    //                     comments = comments+comment.snippet.topLevelComment.snippet.textOriginal;
-    //                 comments = comments+'</p>';
-    //             comments = comments+'</div>';
-    //         comments = comments+'</div>';
-    //     comments = comments+'</li>';
-
-    //     return comments;
-    // }
+    
 
 </script>
